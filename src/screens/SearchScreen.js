12cac51d8ed45e75b037302b59e75b16b5a3a099
Gulbeyaz <glbzbayram@gmail.com>
2024-05-11 23:05:32 +0300

    Results was called by a component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
 import SearchBar from "../../components/compounds/SearchBar";
+import ResultsList from "../../components/compounds/ResultsList";
+import useResult from "../../hooks/useResult";
 
 export default function SearchScreen() {
+  const [searchApi, results, errorMessage] = useResult();
+  const [term, setTerm] = useState("");
+
+  const filterRestaurantsByPrice = (price) => {
+    return results.filter((r) => {
+      return r.price === price;
+    });
+  };
+
   return (
-    <View>
-      <SearchBar />
-    </View>
+    <ScrollView>
+      <SearchBar
+        changeTermHandle={setTerm}
+        submitHandle={() => searchApi(term)}
+        term={term}
+      />
+      {errorMessage ? (
+        <Text>{errorMessage}</Text>
+      ) : (
+        <>
+          {results.length > 0 ? (
+            <>
+              <ResultsList
+                title="En Uygun Restoranlar"
+                results={filterRestaurantsByPrice("₺")}
+              />
+              <ResultsList
+                title="Uygun Restoranlar"
+                results={filterRestaurantsByPrice("₺₺")}
+              />
+              <ResultsList
+                title="Pahalı Restoranlar"
+                results={filterRestaurantsByPrice("₺₺₺")}
+              />
+              <ResultsList
+                title="En Pahalı Restoranlar"
+                results={filterRestaurantsByPrice("₺₺₺₺")}
+              />
+            </>
+          ) : (
+            <Text>Hiç bir sonuç bulunamadı.</Text>
+          )}
+        </>
+      )}
+    </ScrollView>
   );
 }
 
